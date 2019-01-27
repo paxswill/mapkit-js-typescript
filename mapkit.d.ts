ccb20c31a72521715015e4ac44dc3a2f0d3c0a3d
Will Ross <paxswill@paxswill.com>
2019-01-26 20:21:07 -0500

    Add documentation for Padding fields

--- mapkit.d.ts
+++ mapkit.d.ts
@@ -483,9 +483,17 @@ interface PaddingConstructorOptions
 class Padding
 {
     constructor(options?: PaddingConstructorOptions|number[] )
+
+    /** The amount of padding, in CSS pixels, to inset the map from the bottom edge. */
     bottom: number
+
+    /** The amount of padding, in CSS pixels, to inset the map from the left edge. */
     left: number
+
+    /** The amount of padding, in CSS pixels, to inset the map from the right edge. */
     right: number
+
+    /** The amount of padding, in CSS pixels, to inset the map from the top edge. */
     top: number
 }
 
